39fe2ef3de169a6aec349d8a521b024335c18f9d
SamirSales <samir.sribeiro@gmail.com>
2018-09-04 14:38:40 -0300

    messages of validation via files

--- src/main/java/io/github/samirsales/curso/config/SpringMvcConfig.java
+++ src/main/java/io/github/samirsales/curso/config/SpringMvcConfig.java
@@ -1,7 +1,9 @@
 package io.github.samirsales.curso.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -24,5 +26,12 @@ public class SpringMvcConfig implements WebMvcConfigurer{
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
 		registry.addConverter(new GenderConverter());
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename("messages");
+		return source;
 	}
 }
